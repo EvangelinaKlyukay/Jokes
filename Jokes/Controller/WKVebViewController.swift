6b3103199169180f6bf62e40aeb0e355034aeccc
Klyukay Evangelina <klyukay.evangelina@gmail.com>
2021-01-25 13:34:27 +0300

    Контроллер второго экрана

--- Jokes/Controller/WKVebViewController.swift
+++ Jokes/Controller/WKVebViewController.swift
@@ -6,3 +6,38 @@
 //
 
 import Foundation
+import WebKit
+
+class WKVebViewController: UIViewController {
+    
+    private var webView: WKWebView?
+
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        webView = WKWebView(frame: view.bounds)
+        view.addSubview(webView!)
+        
+        NSLayoutConstraint.activate([
+            webView!.topAnchor.constraint(equalTo: view!.topAnchor),
+            webView!.leadingAnchor.constraint(equalTo: view!.leadingAnchor),
+            webView!.bottomAnchor.constraint(equalTo: view!.bottomAnchor),
+            webView!.trailingAnchor.constraint(equalTo: view!.trailingAnchor)
+        ])
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        webView?.loadUrl(string: "http://www.icndb.com/api/")
+    }
+}
+
+extension WKWebView {
+    func loadUrl(string: String) {
+        if let url = URL(string: string) {
+            load(URLRequest(url: url))
+        }
+    }
+}
